81926b112b4fdf9de94e6513fabd6ed7c246ad73
Clément Roussel <clement.roussel@onf.fr>
2022-07-20 20:47:54 +0200

    updates

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'GML', 'author': 'Clement Roussel', 'version': '22.07.0', 'environment': 'local'}
